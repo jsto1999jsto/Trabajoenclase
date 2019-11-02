fa9e9a24b61f2ad2964b5d2ee88c471c0641478d
JoanTovar <jstovar94@misena.edu.co>
2019-11-01 19:11:22 -0500

    Version 2

--- Operaciones/src/operaciones/Operaciones.java
+++ Operaciones/src/operaciones/Operaciones.java
@@ -1,10 +1,14 @@
 
 package operaciones;
 
+import java.util.Scanner;
+
 public class Operaciones {
 
     public static void main(String[] args) {
-  
+  Scanner opc = new Scanner(System.in);
+  System.out.println("Digite su nombre");
+  String nombre = opc.nextLine();
     }
     
 }
