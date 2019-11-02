1c83dc769f04e72eb7a220be254b60317b29c7c0
JoanTovar <jstovar94@misena.edu.co>
2019-11-01 20:27:26 -0500

    version2 sebastian

--- Operaciones/src/operaciones/Operaciones.java
+++ Operaciones/src/operaciones/Operaciones.java
@@ -6,12 +6,17 @@ import java.util.Scanner;
 public class Operaciones {
 
     public static void main(String[] args) {
-        String  nombre,apellido;
+        String  nombre,apellido,direccion;
+        int edad;
   Scanner opc = new Scanner(System.in);
   System.out.println("Digite su nombre");
   nombre = opc.nextLine();
   System.out.println("digite su apellido");
   apellido = opc.nextLine();
+  System.out.println("digite su direccion");
+  direccion = opc.nextLine();
+   System.out.println("digite su edad");
+   edad = opc.nextInt();
   
     }
     
