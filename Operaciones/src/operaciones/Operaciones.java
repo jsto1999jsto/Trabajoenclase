6f108976d49cbb084c5a403ba04647667b3b2fd8
JoanTovar <jstovar94@misena.edu.co>
2019-11-01 20:21:03 -0500

    actualizacion de sebastian

--- Operaciones/src/operaciones/Operaciones.java
+++ Operaciones/src/operaciones/Operaciones.java
@@ -6,9 +6,13 @@ import java.util.Scanner;
 public class Operaciones {
 
     public static void main(String[] args) {
+        String  nombre,apellido;
   Scanner opc = new Scanner(System.in);
   System.out.println("Digite su nombre");
-  String nombre = opc.nextLine();
+  nombre = opc.nextLine();
+  System.out.println("digite su apellido");
+  apellido = opc.nextLine();
+  
     }
     
 }
